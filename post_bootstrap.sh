f01954067c0d68140af5d02e6e303d2ecd85fbae
esxilli <Shuixi.Li@ericsson.com>
2016-01-04 18:49:42 +0000

    sshrc commit

--- post_bootstrap.sh
+++ post_bootstrap.sh
@@ -4,6 +4,10 @@ ln -s /home/vagrant/dotfiles/tmux.conf /home/vagrant/.tmux.conf
 ln -s /home/vagrant/dotfiles/config /home/vagrant/.config
 ln -s /home/vagrant/dotfiles/vimrc /home/vagrant/.vimrc
 ln -s /home/vagrant/dotfiles/zshrc /home/vagrant/.zshrc
-
 ln -s /home/vagrant/dotfiles/bashrc /home/vagrant/.bashrc
 ln -s /home/vagrant/dotfiles/gitconfig /home/vagrant/.gitconfig
+ln -s /home/vagrant/dotfiles/sshrc /home/vagrant/.sshrc
+
+ln -s /home/vagrant/dotfiles/tmux.conf /home/vagrant/.sshrc.d/.tmux.conf
+#ln -s /home/vagrant/dotfiles/vimrc /home/vagrant/.sshrc.d/.vimrc
+ln -s /home/vagrant/dotfiles/bashrc /home/vagrant/.sshrc.d/.bashrc
