8fa95da20adf19d0b47639f2cf995d24ed8e2dd9
Shuixi.Li <Shuixi.Li@ca.imsbrogan.com>
2017-11-16 20:32:47 -0500

    mac path correction

---
+++ mac/mac_post_bootstrap.sh
@@ -0,0 +1,10 @@
+ln -s /Users/shuixi.li/git/dotfiles/mac/tmux.conf ~/.tmux.conf
+ln -s /Users/shuixi.li/git/dotfiles/mac/vimrc ~/.vimrc
+ln -s /Users/shuixi.li/git/dotfiles/mac/zshrc ~/.zshrc
+ln -s /Users/shuixi.li/git/dotfiles/mac/bashrc ~/.bashrc
+ln -s /Users/shuixi.li/git/dotfiles/gitconfig ~/.gitconfig
+ln -s /Users/shuixi.li/git/dotfiles/mac/sshrc ~/.sshrc
+
+ln -s /Users/shuixi.li/git/dotfiles/mac/tmux.conf ~/.sshrc.d/.tmux.conf
+#ln -s /Users/shuixi.li/git/dotfiles/mac/vimrc ~/.sshrc.d/.vimrc
+ln -s /Users/shuixi.li/git/dotfiles/mac/bashrc ~/.sshrc.d/.bashrc
